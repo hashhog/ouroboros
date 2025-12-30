f4583c19fe3a5c58a2d66693f4c1fdb1ee71e57b
max <maxtclinton@gmail.com>
2025-12-30 15:59:13 -0500

    Add varint encode/decode, serilization for wrappers

--- ferrous-utils/common/src/lib.rs
+++ ferrous-utils/common/src/lib.rs
@@ -2,6 +2,7 @@
 
 pub mod types;
 pub mod crypto;
+pub mod serialize;
 
 // Re-export all types for convenience
 pub use types::{
@@ -13,4 +14,11 @@ pub use types::{
 pub use crypto::{
     verify_ecdsa_signature, double_sha256, hash160, compute_merkle_root,
     bits_to_target, target_to_bits,
+};
+
+// Re-export serialization functions
+pub use serialize::{
+    BitcoinSerialize, BitcoinDeserialize, SerializeError,
+    encode_varint, decode_varint,
+    serialize_to_vec, deserialize_from_slice,
 };
