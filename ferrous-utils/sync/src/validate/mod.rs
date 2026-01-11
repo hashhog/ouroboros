ee138970cd72a29af41baab2a64121d7d9a63a6d
max <maxtclinton@gmail.com>
2026-01-11 09:16:02 -0500

    Add block validation

--- ferrous-utils/sync/src/validate/mod.rs
+++ ferrous-utils/sync/src/validate/mod.rs
@@ -4,9 +4,11 @@ pub mod header;
 pub mod pow;
 pub mod transaction;
 pub mod script;
+pub mod block;
 
 // Re-export functions for convenience
 pub use header::{HeaderValidator, HeaderValidationError};
 pub use pow::*;
 pub use transaction::*;
 pub use script::{ScriptInterpreter, ScriptError, ScriptType, Stack, identify_script_type, verify_signature_in_script, verify_witness};
+pub use block::{BlockValidator, BlockValidationError};
